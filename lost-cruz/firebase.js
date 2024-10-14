db1c4b839221b5770ccf978b98bf9e6e1213bca6
Thomas Wong <147549325+TheSpartanBuilder@users.noreply.github.com>
2024-10-14 10:32:27 -0700

    FireConfig

--- lost-cruz/firebase.js
+++ lost-cruz/firebase.js
@@ -4,21 +4,18 @@ import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//Learn how to import firebase api from file to protect api information in github
-/*
-const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
-};
-*/
+import fireConfig from './fireConfig.json'
 
-import firebaseConfig from "./firebase.config.js"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: fireConfig.apiKey,
+    authDomain: fireConfig.authDomain,
+    projectId: fireConfig.projectId,
+    storageBucket: fireConfig.storageBucket,
+    messagingSenderId: fireConfig.messagingSenderId,
+    appId: fireConfig.appId,
+    measurementId: fireConfig.measurementId
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
