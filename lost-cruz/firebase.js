2af4c7875643c0fc675cb12078eca4aefb0df984
Thomas Wong <147549325+TheSpartanBuilder@users.noreply.github.com>
2024-10-14 10:28:00 -0700

    Revert "fireConfig work"
    This reverts commit e920cc13826e3336aa76a939b56df233484076eb.

--- lost-cruz/firebase.js
+++ lost-cruz/firebase.js
@@ -4,18 +4,21 @@ import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import fireConfig from './fireConfig.json'
-
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//Learn how to import firebase api from file to protect api information in github
+/*
 const firebaseConfig = {
-    apiKey: fireConfig.apiKey,
-    authDomain: fireConfig.authDomain,
-    projectId: fireConfig.projectId,
-    storageBucket: fireConfig.storageBucket,
-    messagingSenderId: fireConfig.messagingSenderId,
-    appId: fireConfig.appId,
-    measurementId: fireConfig.measurementId
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
+*/
+
+import firebaseConfig from "./firebase.config.js"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
