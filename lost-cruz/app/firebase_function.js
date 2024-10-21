91b1c632ede07cc0f494ff1c04d53085bac91ea8
Thomas Wong <147549325+TheSpartanBuilder@users.noreply.github.com>
2024-10-21 11:04:17 -0700

    Adding some functions for reference

--- lost-cruz/app/firebase_function.js
+++ lost-cruz/app/firebase_function.js
@@ -8,4 +8,37 @@ import {
   deleteDoc,
   getDoc,
 } from 'firebase/firestore'
+import {doc, setDoc, collection} from "firebase/firestore"
+import {db} from "..\firebase.js"
+import { serverTimestamp } from 'firebase/firestore';
 
+// Reference:
+// https://www.youtube.com/watch?v=wHKopeB8WMg&list=PL391NzrHDDl8N5Y-QZ8CoCPT3NXieduIT&index=11
+
+  export const createPost = async (the_post) =>{
+    const collectionRef = collection(db, "post");
+    const postRef = doc(collection(db,"post"), the_post);
+    const docSnap = await getDoc(docRef);
+    if (text.current.value != ""){
+      try{
+        await setDoc(postRef, {
+          documentID: document,
+          title: title,
+          text: text.current.value,
+          image: image,
+          email: email,
+          location: location,
+          timestamp: serverTimestamp()
+        });
+        text.current.value = "";
+      } catch(err) {
+        alert(err.message);
+        console.log(err.message);
+        return false;
+      }
+    }
+    else
+    {
+      return false;
+    }
+  }
