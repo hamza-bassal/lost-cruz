143b30cbf9f7c7c30e6991bc79eb0f4a0759a826
Lfer58 <nishitd20@gmail.com>
2024-10-26 14:41:35 -0700

    Main page is now the welcome page

--- lost-cruz/app/page.js
+++ lost-cruz/app/page.js
@@ -1,15 +1,17 @@
 'use client'
 
 import ForumPage from "./forum/page";  // Capitalize the component name
+import Home from "./welcome/page"
 
 // Alternatively, define the component explicitly
 const ForumPageComponent = () => <ForumPage />;  // Correct arrow function syntax
+const HomePageComponent = () => <Home />
 
-export default function Home() {
+export default function HomePage() {
     return (
         <div>
             {/* Render the ForumPage component */}
-            <ForumPageComponent />
+            <HomePageComponent />
 
             {/* for testing purposes: */}
             {/*<Home />*/}
