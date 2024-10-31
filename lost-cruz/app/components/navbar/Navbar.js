3a8cc58ce653f5448ee95bf8f496092f0696db70
jhu343 <jiayi.h622@gmail.com>
2024-10-30 18:06:07 -0700

    move logout to navbar

--- lost-cruz/app/components/navbar/Navbar.js
+++ lost-cruz/app/components/navbar/Navbar.js
@@ -1,76 +1,82 @@
 'use client'
 
-import { Box, FormGroup, FormControlLabel, Checkbox, IconButton, Link } from "@mui/material";
+import { Box, FormGroup, FormControlLabel, Checkbox, IconButton, Link, Button } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
 import styles from "./navbar.module.css";
 import { useState } from "react";
-// import { useNavigate } from "react-router-dom"
+import useLogout from "@/app/hooks/useLogout"
 
-const Search = () => {
-    return (
-        <div className={styles.searchWrapper}>
-            <form className={styles.searchForm} method="get" action="/">
-                <input className={styles.searchBar} type="text" placeholder="Search" />
-                <IconButton>
-                    <SearchIcon sx={{ color: '#0174BE' }} />
-                </IconButton>
-            </form>
-        </div>
-    )
-}
-
-const MenuBtn = () => {
-    const [open, setOpen] = useState(false)
-    return (
-        <div className={styles.dropdown}>
-            <IconButton>
-                <MenuIcon
-                    fontSize="large"
-                    onClick={() => {
-                        setOpen(prev => !prev)
-                    }}
-                    sx={{
-                        color: '#FCF7ED',
-                        padding: '0px',
-                    }} />
-            </IconButton>
-
-            {open && <div className={styles.dropdownBox}>
-                <FormGroup row>
-                    <FormControlLabel control={<Checkbox defaultChecked size="small" />} label="Lost" />
-                    <FormControlLabel control={<Checkbox defaultChecked size="small" />} label="Found" />
-                </FormGroup>
-                <hr className={styles.hr} />
-                <FormGroup row>
-                    <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
-                    <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
-                    <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
-                    <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
-                    <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
-                    <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
-                    <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
-                    <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
-                    <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
-                </FormGroup>
-            </div>}
-        </div>
-    )
-}
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false); // filter
+    const [prof, setProf] = useState(false); // profile
+
+    const { handleLogout, isLoggingOut, error } = useLogout();
+
+    const MenuBtn = () => {
+        return (
+            <div className={styles.dropdown}>
+                <IconButton>
+                    <MenuIcon
+                        fontSize="large"
+                        onClick={() => {
+                            setOpen(prev => !prev);
+                            setProf(false);
+                        }}
+                        sx={{
+                            color: '#FCF7ED',
+                            padding: '0px',
+                        }} />
+                </IconButton>
+
+                {open && <div className={styles.dropdownBox}>
+                    <FormGroup row>
+                        <FormControlLabel control={<Checkbox defaultChecked size="small" />} label="Lost" />
+                        <FormControlLabel control={<Checkbox defaultChecked size="small" />} label="Found" />
+                    </FormGroup>
+                    <hr className={styles.hr} />
+                    <FormGroup row>
+                        <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
+                        <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
+                        <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
+                        <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
+                        <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
+                        <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
+                        <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
+                        <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
+                        <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }} />} label="element" />
+                    </FormGroup>
+                </div>}
+            </div>
+        )
+    }
+
+    const Search = () => {
+        return (
+            <div className={styles.searchWrapper}>
+                <form className={styles.searchForm} method="get" action="/">
+                    <input className={styles.searchBar} type="text" placeholder="Search" />
+                    <IconButton>
+                        <SearchIcon sx={{ color: '#0174BE' }} />
+                    </IconButton>
+                </form>
+            </div>
+        )
+    }
+
     return (
         <Box className={styles.navBar}
-        sx={{
-            position: 'fixed',
-            top: 0,
-            zIndex: 10,
-            bgcolor: '#0174BE',
-            height: '50px',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-around',
-        }}>
+            sx={{
+                position: 'fixed',
+                top: 0,
+                zIndex: 10,
+                bgcolor: '#0174BE',
+                height: '50px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-around',
+            }}>
             {/* Logo */}
             <Link href={`/forum`}>
                 <Box
@@ -103,7 +109,12 @@ const Navbar = () => {
                 <Box sx={{
                     width: '50px',
                     height: '50px',
-                }}>
+                }}
+                    onClick={() => {
+                        setProf(prev => !prev);
+                        setOpen(false);
+                    }}
+                >
                     <Box
                         sx={{
                             width: '80%',
@@ -116,6 +127,18 @@ const Navbar = () => {
                         {/* Profile Image */}
                     </Box>
                 </Box>
+
+                {
+                    prof &&
+                    <Box className={styles.profdropdownBox}>
+                        <Box>
+                            <Button href={"/profile"}>Profile</Button>
+                        </Box>
+                        <Box>
+                            <Button onClick={handleLogout}>Logout</Button>
+                        </Box>
+                    </Box>
+                }
             </Box>
         </Box>
     );
