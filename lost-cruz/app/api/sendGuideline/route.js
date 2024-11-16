912d8787d537af09bfc849a5a0dca04a35738622
Lfer58 <nishitd20@gmail.com>
2024-11-16 00:05:33 -0800

    more email updates

--- lost-cruz/app/api/sendGuideline/route.js
+++ lost-cruz/app/api/sendGuideline/route.js
@@ -25,6 +25,8 @@ export async function POST(request) {
     // Configure Nodemailer transporter
     const transporter = nodemailer.createTransport({
         service: 'gmail',
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.MAIL_PASSWORD,
