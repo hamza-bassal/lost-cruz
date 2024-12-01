75825b69b1f048eab46e3b4fc68b34a6e2d79c03
Lfer58 <nishitd20@gmail.com>
2024-12-01 00:31:58 -0800

    Removed forced digest send

--- lost-cruz/app/components/navbar/Navbar.js
+++ lost-cruz/app/components/navbar/Navbar.js
@@ -322,9 +322,9 @@ const Navbar = ({ setSearch, setLostStatus, isForum = false}) => {
                         <Box>
                             <Button onClick={handleLogout}>Logout</Button>
                         </Box>
-                        <Box>
+                        {/* <Box>
                             <Button sx={{ color: '#FFC436' }} onClick={handleSubmit}>E-test</Button>
-                        </Box>
+                        </Box> */}
                     </Box>
                 }
             </Box>
